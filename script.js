2b4e206ea1decdfd5d42c1290c5acc0135b33a5b
nrk317 <annie54315431@gmail.com>
2026-01-05 16:29:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
 (function () {
   const links = Array.from(document.querySelectorAll('.nav__link'));
-  const sections = links
-    .map(a => document.querySelector(a.getAttribute('href')))
-    .filter(Boolean);
-
-  function setActive(id) {
-    links.forEach(a => {
-      const isActive = a.getAttribute('href') === `#${id}`;
-      a.classList.toggle('is-active', isActive);
-    });
-  }
-
-  const obs = new IntersectionObserver((entries) => {
-    const visible = entries
-      .filter(e => e.isIntersecting)
-      .sort((a, b) => b.intersectionRatio - a.intersectionRatio)[0];
-
-    if (visible?.target?.id) setActive(visible.target.id);
-  }, {
-    threshold: [0.15, 0.25, 0.35, 0.5],
-    rootMargin: "-20% 0px -70% 0px"
+  const here = location.pathname.split('/').pop() || 'index.html';
+  links.forEach(a => {
+    const href = a.getAttribute('href');
+    if (href && href.endsWith(here)) a.classList.add('is-active');
   });
-
-  sections.forEach(sec => obs.observe(sec));
 })();
